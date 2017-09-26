29a7d175afc91c78cd39b4976d1cca1a8cce69c8
Austin Stewart <hello@austinthedeveloper.com>
2017-09-26 10:10:37 -1000

    Built a simple count-to directive

--- public_api.ts
+++ public_api.ts
@@ -3,10 +3,10 @@
  * Build an Angular library compatible with AoT compilation & Tree shaking.
  * Written by Roberto Simonetti.
  * MIT license.
- * https://github.com/robisim74/angular-library-starter
+ * https://github.com/robisim74/angular-count-to
  */
 
 /**
  * Entry point for all public APIs of the package.
  */
-export * from './src/angular-library-starter';
+export * from './src/angular-count-to';
---
+++ src/angular-count-to.ts
@@ -0,0 +1,3 @@
+// Public classes.
+export { CountToDirective } from './directives/count-to.directive';
+export { CountToModule } from './modules/count-to.module';
